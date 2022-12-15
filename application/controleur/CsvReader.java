b59279e8e68a054a6b84ccbe99ee466a771c553d
Yupyup <99692735+eliesav@users.noreply.github.com>
2022-12-15 16:06:05 +0100

    test si ça amrche

--- application/controleur/CsvReader.java
+++ application/controleur/CsvReader.java
@@ -2,19 +2,34 @@ package application.controleur;
 
 import java.io. * ;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.*;
 
 //CSVReader.liseurCsv("path")
 
 public abstract class CsvReader {
-  public static void liseurCsv (String argument) {
-    Scanner sc = new Scanner(new File(argument),"utf-8");
+  public static void liseurCsv (String argument) throws FileNotFoundException{
+    Scanner sc = new Scanner(new File (argument),"utf-8");
     //parsing a CSV file into the constructor of Scanner class 
     sc.useDelimiter(",");
     //setting comma as delimiter pattern
     while (sc.hasNext()) {
       //récupérer variable qui contient le login écrit
       //if le login entré == sc alors stop et ouvre le frame
-      System.out.print(sc.next()+"\t");
+      Map<Integer, String> larousse = new HashMap<Integer,String>();
+      Pattern p_fin = Pattern.compile(".+[\n\r]+");
+      String chiant = sc.next();
+      Matcher input = p_fin.matcher(chiant);
+      String temporaire = "";
+      if (input.find()){
+        System.out.print("bskgfksfdjlg");
+        // temporaire="";
+      }else{
+        System.out.print(chiant);
+        // temporaire += sc.next();
+    }
+      // 
     }
     sc.close();
     //closes the scanner  
---
+++ application/controleur/Test.java
@@ -0,0 +1,8 @@
+package application.controleur;
+import java.io. * ;
+
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        CsvReader.liseurCsv("./application/data/dataeleve.csv");
+    }
+}
