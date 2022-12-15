8cc424435a3e02133a1ab9f072e1a1585e67df38
Yupyup <99692735+eliesav@users.noreply.github.com>
2022-12-15 17:06:19 +0100

    C'EST BON LE CSV MARCHE YEAAAAH

--- application/controleur/CsvReader.java
+++ application/controleur/CsvReader.java
@@ -13,24 +13,30 @@ public abstract class CsvReader {
     Scanner sc = new Scanner(new File (argument),"utf-8");
     //parsing a CSV file into the constructor of Scanner class 
     sc.useDelimiter(",");
+    Map<String, String> larousse = new HashMap<String,String>();
+    Pattern p_fin = Pattern.compile(".+[\n\r]+");
+    String temporaire = "";
+    String login = "";
     //setting comma as delimiter pattern
     while (sc.hasNext()) {
       //récupérer variable qui contient le login écrit
       //if le login entré == sc alors stop et ouvre le frame
-      Map<Integer, String> larousse = new HashMap<Integer,String>();
-      Pattern p_fin = Pattern.compile(".+[\n\r]+");
       String chiant = sc.next();
       Matcher input = p_fin.matcher(chiant);
-      String temporaire = "";
-      if (input.find()){
-        System.out.print("bskgfksfdjlg");
-        // temporaire="";
-      }else{
-        System.out.print(chiant);
-        // temporaire += sc.next();
+      if (chiant.equals("Login") || chiant.equals("Nom") || chiant.equals("Prenom") || chiant.contains("Langues")){
+        continue;
+      }else if (input.find()){
+        temporaire += chiant;
+        larousse.put(login,temporaire);
+        temporaire="";
+      }else if (Character.isDigit(chiant.charAt(0))){
+      login = "";
+      login = chiant;
+    } else if (!chiant.equals(input.find())){
+        temporaire += chiant + " ";
     }
-      // 
     }
+    System.out.println(larousse);
     sc.close();
     //closes the scanner  
   }
